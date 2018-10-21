e290c78b4ecd38e901be433531524d483ee53309
NolanMullins <nolan.mullins@me.com>
2018-10-21 11:53:02 -0400

    updated run script

--- a2/run.sh
+++ a2/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # ip:port file <number of time to run>
-if (($# != 3)); 
+if (($# < 3)); 
 then
-    echo "<IP:Port> <file> <# of runs> required"
+    echo "<IP:Port> <file> <# of runs> <transfer size>"
     exit -1
 fi
 for ((i=0; i<$3; i++))
 do
-    ./client $1 $2
+    ./client $1 $2 $4
 done
